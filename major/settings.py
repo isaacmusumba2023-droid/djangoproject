35260215d10a5553b9f1fb1ebf14789be26afda9
MUSUMBA ISAAC <isaacmusumba2023@gmail.com>
2026-08-09 10:53:49 +0300

    Update ALLOWED_HOSTS for Render deployment

--- major/settings.py
+++ major/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x2g_#84b^iwk!%o6t6^ypu2(67f*cu2ad&kd=z1_o*p*9gljg8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangoproject-74fd.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
